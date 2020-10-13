502015425c71306a28484d11fb8fcf96dcef189b
Forthus <samwellforthus@outlook.com>
2020-10-13 23:38:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import discord
+import logging
 from discord.ext import commands
 from os import environ
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 client = commands.Bot(
     command_prefix="-",
@@ -19,10 +26,5 @@ async def on_ready():
     for cog in cogs:
         client.load_extension(cog)
     return
-
-@client.event
-async def on_message(message):
-    print(f"{message.created_at} - {message.author.name}#{message.author.discriminator}: {message.content}")
-    await client.process_commands(message)
 
 client.run(environ["DISCORD_TOKEN"])
